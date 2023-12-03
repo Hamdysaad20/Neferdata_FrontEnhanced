d96a61b86473686fddace6641ce2174f9966c53d
hamdysaad20 <hamdysaad.biz@gmail.com>
2023-12-04 00:54:05 +0200

    init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import React from "react";
+import {CircularProgress} from "@nextui-org/react";
+
+export default function App() {
+  return (
+    <div className="  justify-center flex m-auto">
+           <CircularProgress label="Loading..." />
+
+    </div>
+
+  );
+}
